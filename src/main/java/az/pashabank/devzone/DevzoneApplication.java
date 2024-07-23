15d40bade5e1a4e5cbbeacec78ce569090e32a79
Pərvin Yaqubzadə <parvin.yagubzade@pashabank.az>
2024-07-24 03:11:50 +0400

    Create empty spring application

---
+++ src/main/java/az/pashabank/devzone/DevzoneApplication.java
@@ -0,0 +1,13 @@
+package az.pashabank.devzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevzoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevzoneApplication.class, args);
+    }
+
+}
