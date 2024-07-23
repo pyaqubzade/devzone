459f08366221ed04a3c435ac8bca7f0cd651284b
Pərvin Yaqubzadə <parvin.yagubzade@pashabank.az>
2024-07-24 03:11:50 +0400

    Initial commit

---
+++ src/test/java/az/pashabank/devzone/DevzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package az.pashabank.devzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevzoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
